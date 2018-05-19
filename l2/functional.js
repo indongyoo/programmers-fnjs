74fdaa760bd5a169f05d398f69215c72b36f0214
Indongyoo <idy@marpple.com>
2018-05-19 15:28:19 +0900

    cc

---
+++ l2/functional.js
@@ -0,0 +1,139 @@
+const identity = a => a;
+
+const collIter = coll =>
+  coll.constructor == Object ?
+    valuesIter(coll) :
+    coll[Symbol.iterator]();
+
+function *valuesIter(obj) {
+  for (const k in obj) yield obj[k];
+}
+
+function *entriesIter(obj) {
+  for (const k in obj) yield [k, obj[k]];
+}
+
+function *reverseIter(arr) {
+  var l = arr.length;
+  while (l--) yield arr[l];
+}
+
+function reduce(f, coll, acc) {
+  const iter = collIter(coll);
+  acc = acc === undefined ? iter.next().value : acc;
+  for (const v of iter) {
+    acc = f(acc, v);
+  }
+  return acc;
+}
+
+const countBy = (f, coll) => reduce((counts, a) => incSel(counts, f(a)), coll, {});
+
+const count = coll => countBy(identity, coll);
+
+const groupBy = (f, coll) => reduce((group, a) => pushSel(group, f(a), a), coll, {});
+
+function incSel(parent, k) {
+  parent[k] ? parent[k]++ : parent[k] = 1;
+  return parent;
+}
+
+function pushSel(parent, k, v) {
+  (parent[k] || (parent[k] = [])).push(v);
+  return parent;
+}
+
+console.log(
+  reduce((acc, a) => acc + a, [1, 2, 3], 0)
+);
+
+console.log(
+  reduce((acc, a) => acc + a, { a: 1, b: 2, c: 3 }, 0)
+);
+
+console.log(
+  reduce((a, b) => a - b, reverseIter([1, 2, 3, 4]))
+);
+
+const posts = [
+  { id: 1, body: '내용1', comments: [{}, {}] },
+  { id: 2, body: '내용2', comments: [{}] },
+  { id: 3, body: '내용3', comments: [{}, {}, {}] },
+  { id: 4, body: '내용4', comments: [{}, {}, {}] },
+];
+
+// 모든 posts를 통해서 comments의 총 수를 얻어주세요.
+
+console.log(
+  reduce((count, p) => count + p.comments.length, posts, 0)
+);
+
+const users = [
+  { id: 1, name: 'name1', age: 30 },
+  { id: 2, name: 'name2', age: 31 },
+  { id: 3, name: 'name3', age: 32 },
+  { id: 4, name: 'name4', age: 31 },
+  { id: 5, name: 'name5', age: 32 }
+];
+
+console.log(
+  reduce((group, u) => pushSel(group, u.age, u), users, {})
+);
+
+
+console.log( groupBy(u => u.age, users) );
+
+const res = {
+  30: [{ id: 1, name: 'name1', age: 30 }],
+  31: [{ id: 2, name: 'name2', age: 31 }, { id: 4, name: 'name4', age: 31 }],
+  32: [{ id: 3, name: 'name3', age: 32 }, { id: 5, name: 'name5', age: 32 }]
+};
+
+
+
+reduce((counts, a) => incSel(counts, a.age), users, {});
+
+console.log(
+  countBy(u => u.age, users)
+);
+
+console.log(
+  countBy(identity, [1, 2, 3, 3, 4, 5, 5, 10, 10, 10])
+);
+
+console.log(
+  count([1, 2, 3, 3, 4, 5, 5, 10, 10, 10])
+);
+
+console.log(
+  groupBy(identity, [1, 2, 3, 3, 4, 5, 5, 10, 10, 10])
+);
+
+
+const res2 = {
+  30: 1,
+  31: 2,
+  32: 2
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
